277adf45310b04593e0070c1c6be6631861fda16
iflyrana <rana.vanikar@gmail.com>
2023-03-12 18:02:24 +0530

    added a do while loop for running simplicity

--- 2funcoverloading.cpp
+++ 2funcoverloading.cpp
@@ -18,47 +18,53 @@ float area(float base, float height, float dummy) {
 }
 
 void displayMenu() {
-        cout << "Enter your choice:\n";
+    cout << "Enter your choice:\n";
     cout << "1. Area of Circle\n";
     cout << "2. Area of Square\n";
     cout << "3. Area of Triangle\n";
     cout << "4. Area of Rectangle\n";
+    cout << "0. Exit\n";
 }
 
 int main() {
     int choice, side;
     float radius, base, height;
+
+    do {
     displayMenu();
     cin >> choice;
 
     switch(choice) {
-        case 1:
-            cout << "Enter radius of circle: ";
-            cin >> radius;
-            cout << "Area of circle is " << area(radius) << endl;
-            break;
-        case 2:
-            cout << "Enter side of square: ";
-            cin >> side;
-            cout << "Area of square is " << area(side) << endl;
-            break;
-        case 3:
-            cout << "Enter base of triangle: ";
-            cin >> base;
-            cout << "Enter height of triangle: ";
-            cin >> height;
-            cout << "Area of triangle is " << area(base, height) << endl;
-            break;
-        case 4:
-            cout << "Enter base of rectangle: ";
-            cin >> base;
-            cout << "Enter height of rectangle: ";
-            cin >> height;
-            cout << "Area of rectangle is " << area(base, height, 0) << endl;
-            break;
-        default:
-            cout << "Invalid choice!" << endl;
+            case 1:
+                cout << "Enter radius of circle: ";
+                cin >> radius;
+                cout << "Area of circle is " << area(radius) << endl;
+                break;
+            case 2:
+                cout << "Enter side of square: ";
+                cin >> side;
+                cout << "Area of square is " << area(side) << endl;
+                break;
+            case 3:
+                cout << "Enter base of triangle: ";
+                cin >> base;
+                cout << "Enter height of triangle: ";
+                cin >> height;
+                cout << "Area of triangle is " << area(base, height) << endl;
+                break;
+            case 4:
+                cout << "Enter base of rectangle: ";
+                cin >> base;
+                cout << "Enter height of rectangle: ";
+                cin >> height;
+                cout << "Area of rectangle is " << area(base, height, 0) << endl;
+                break;
+            case 0:
+                cout << "Exit" << endl;
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
     }
-
+    }while(choice != 0);
     return 0;
 }
